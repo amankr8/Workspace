d4828f268d972077c52fe96763dde525d9cc6efc
Aman Kumar <rx8aman@gmail.com>
2021-06-17 12:21:29 -0700

    new files and snippets added

--- archives/leetcode/1248.cpp
+++ archives/leetcode/1248.cpp
@@ -5,12 +5,12 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int> &nums, int k) {
         int count = 0;
+        unordered_map<int, int> m{{0, 1}};
+        int odd = 0;
         for(int i=0; i<nums.size(); i++) {
-            int odd = 0;
-            for(int j=i; j<nums.size(); j++) {
-                if(nums[j]%2 == 1) odd++;
-                if(odd == k) count++;
-            }
+            if(nums[i]%2 != 0) odd++;
+            if(m.count(odd-k)) count += m[odd-k];
+            m.count(odd) ? m[odd]++ : m[odd] = 1;
         }
         return count;
     }
--- archives/leetcode/523.cpp
+++ archives/leetcode/523.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int> &nums, int k) {
         int sum = 0;
-        map<int, int> m{{0, -1}};
+        unordered_map<int, int> m{{0, -1}};
         for(int i=0; i<nums.size(); i++) {
             sum += nums[i];
             if(k) sum %= k;
