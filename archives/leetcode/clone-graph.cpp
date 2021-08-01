a40629133991818d07c2fc715c0d817dfb0b15ab
Aman Kumar <rx8aman@gmail.com>
2021-08-01 15:59:36 -0700

    update

---
+++ archives/leetcode/clone-graph.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> m;
+    
+    Node* cloneGraph(Node* node) {
+        if(!node) return NULL;
+        
+        Node* copy = new Node(node->val, {});
+        m[node] = copy;
+        
+        queue<Node*> q;
+        q.push(node);
+        
+        while(!q.empty()) {
+            Node* curr = q.front();
+            
+            for(auto it : curr->neighbors) {
+                if(!m.count(it)) {
+                    m[it] = new Node(it->val, {});
+                    q.push(it);
+                }
+                
+                m[curr]->neighbors.push_back(m[it]);
+            }
+            
+            q.pop();
+        }
+        
+        return copy;
+    }
+};
