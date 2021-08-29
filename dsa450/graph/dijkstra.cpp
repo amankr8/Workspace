7a4c26374d6defeeb6302c41bf594597c73b0f21
Aman Kumar <rx8aman@gmail.com>
2021-08-29 19:24:14 +0530

    added things

---
+++ dsa450/graph/dijkstra.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+void handle_io() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    handle_io();
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<pair<int, int>>> adj(n+1);
+    for(int i=0; i<n; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    int source;
+    cin >> source;
+
+    vector<int> dist(n+1, INT_MAX);
+    dist[source] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, source});
+    while(!pq.empty()) {
+        int node = pq.top().second;
+        int weight = pq.top().first;
+        pq.pop();
+
+        for(pair<int, int> p : adj[node]) {
+            if(weight + p.second < dist[p.first]) {
+                dist[p.first] = weight + p.second;
+                pq.push({dist[p.first], p.first});
+            }
+        }
+    }
+
+    for(int i=1; i<dist.size(); i++) cout << dist[i] << " ";
+    cout << endl;
+
+    return 0;
+}
+
+// int main() {
+//     handle_io();
+
+//     int n, m;
+//     cin >> n >> m;
+
+//     map<int, set<pair<int, int>>> adj;
+//     for(int i=0; i<n; i++) {
+//         int u, v, w;
+//         cin >> u >> v >> w;
+//         adj[u].insert({v, w});
+//         adj[v].insert({u, w});
+//     }
+
+//     int source;
+//     cin >> source;
+
+//     vector<int> dist(n+1, INT_MAX);
+//     dist[source] = 0;
+
+//     set<pair<int, int>> mh;
+//     mh.insert({0, source});
+//     while(!mh.empty()) {
+//         int node = mh.begin()->second;
+//         int weight = mh.begin()->first;
+//         mh.erase({node, weight});
+
+//         for(pair<int, int> p : adj[node]) {
+//             if(weight + p.second < dist[p.first]) {
+//                 dist[p.first] = weight + p.second;
+//                 mh.insert({dist[p.first], p.first});
+//             }
+//         }
+//     }
+
+//     for(int i=1; i<dist.size(); i++) cout << dist[i] << " ";
+//     cout << endl;
+
+//     return 0;
+// }
