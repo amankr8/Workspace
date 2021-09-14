c6ccc5e2877153fb60f1f0f8c614f583cd57fb7d
Aman Kumar <rx8aman@gmail.com>
2021-09-14 23:07:46 +0530

    update

--- archives/interviewbit/cycle-in-directed-graph.cpp
+++ archives/interviewbit/cycle-in-directed-graph.cpp
@@ -1,31 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isCycle(int node, vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& recVisited) {
-    visited[node] = recVisited[node] = true;
+class Solution {
+public:
+    bool isCycle(int node, vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& recVisited) {
+        visited[node] = recVisited[node] = true;
 
-    for(int neighbor : adj[node]) {
-        if(visited[neighbor]) {
-            if(recVisited[node]) return true;
-            continue;
+        for(int neighbor : adj[node]) {
+            if(visited[neighbor]) {
+                if(recVisited[neighbor]) return true;
+                continue;
+            }
+            if(isCycle(neighbor, adj, visited, recVisited)) return true;
         }
-        if(isCycle(neighbor, adj, visited, recVisited)) return true;
+        recVisited[node] = false;
+        return false;
     }
-    recVisited[node] = false;
-    return false;
-}
 
-int Solution::solve(int A, vector<vector<int>> &B) {
-    vector<vector<int>> adj(A + 1);
-    for(int i=0; i<B.size(); i++) {
-        adj[B[i][0]].push_back(B[i][1]);
+    int Solution::solve(int A, vector<vector<int>> &B) {
+        vector<vector<int>> adj(A + 1);
+        for(int i=0; i<B.size(); i++) {
+            adj[B[i][0]].push_back(B[i][1]);
+        }
+        
+        vector<bool> visited(A + 1, false);
+        vector<bool> recVisited(A + 1, false);
+        for(int i=1; i<=A; i++) {
+            if(visited[i]) continue;
+            if(isCycle(i, adj, visited, recVisited)) return true;
+        }
+        return false;
     }
-    
-    vector<bool> visited(A + 1, false);
-    vector<bool> recVisited(A + 1, false);
-    for(int i=1; i<=A; i++) {
-        if(visited[i]) continue;
-        if(isCycle(i, adj, visited, recVisited)) return true;
-    }
-    return false;
-}
+};
--- archives/interviewbit/cycle-in-undirected-graph.cpp
+++ archives/interviewbit/cycle-in-undirected-graph.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isCycle(int parent, int node, vector<vector<int>>& adj, vector<bool>& visited) {
-    visited[node] = true;
-    for(int neighbor : adj[node]) {
-        if(neighbor == parent) continue;
-        if(visited[neighbor]) return true;
-        if(isCycle(node, neighbor, adj, visited)) return true;
-    }
-    return false;
-}
-
 class Solution {
 public:
+    bool isCycle(int parent, int node, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[node] = true;
+        for(int neighbor : adj[node]) {
+            if(neighbor == parent) continue;
+            if(visited[neighbor]) return true;
+            if(isCycle(node, neighbor, adj, visited)) return true;
+        }
+        return false;
+    }
+
     int Solution::solve(int A, vector<vector<int>> &B) {
         vector<vector<int>> adj(A + 1);
         for(int i=0; i<B.size(); i++) {
