18a9493638c61fcfb722291fa5e68d967d2126d0
Aman Kumar <rx8aman@gmail.com>
2021-10-30 20:04:47 +0530

    update

--- functions/math/factorial.cpp
+++ functions/math/factorial.cpp
@@ -1,4 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 // Recursive function to return the factorial of a number
 int factorial(int n) {
     return (n == 1) ? 1 : n * factorial(n - 1);
+}
+
+int mod = pow(10, 9) + 7;
+map<int, int> m;
+int fact(int n) {
+    if(m.count(n)) return m[n];
+    if(n < 2) return 1;
+    m[n] = n * fact(n - 1);
+    return m[n];
 }
